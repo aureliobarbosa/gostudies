78578f1e1c6c9ac23b0d7e44dd9bb641c5bc6d25
Marco Aurélio A. Barbosa <aureliobarbosa@gmail.com>
2023-05-09 15:52:27 -0300

    Added example code for interface - exercise

---
+++ exercises/interfaces/test1.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+type Triangle struct {
+	height float64
+	base   float64
+}
+
+func (t Triangle) getArea() float64 {
+	return t.base * t.height / 2.0
+}
+
+type Square struct {
+	sideLength float64
+}
+
+func (s Square) getArea() float64 {
+	return s.sideLength * s.sideLength
+}
+
+type Shape interface {
+	getArea() float64
+}
+
+func printArea(s Shape) {
+	fmt.Println(s.getArea())
+}
+
+func main() {
+	// var t Triangle
+
+	t := Triangle{5.0, 10.0}
+	s := Square{5.0}
+
+	fmt.Printf("Triangle Area: ")
+	printArea(t)
+
+	fmt.Printf("Square Area: ")
+	printArea(s)
+
+}
