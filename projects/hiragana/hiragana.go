f24e5904c7c5b7c13cfa2bd305738961281206e1
Marco Aurélio A. Barbosa <aureliobarbosa@gmail.com>
2023-10-06 12:38:52 -0300

    Aligned columns labels on hiragana.go

--- projects/hiragana/hiragana.go
+++ projects/hiragana/hiragana.go
@@ -8,7 +8,7 @@ func main() {
 
 	fmt.Printf("%32s %8s %8s %8s %4s\n", "Binary", "Octal", "Integer", "Hexadecimal", "Runa")
 	for runa < 0x30a0 {
-		fmt.Printf("%b %o %d %x %c\n", runa, runa, runa, runa, runa)
+		fmt.Printf("%32b %8o %8d %8x %4c\n", runa, runa, runa, runa, runa)
 		runa++
 	}
 
